e412a134ded034f4a7bf9032887a1a03cb91edae
Alex Lomizov <a.lomizov@gmail.com>
2014-11-22 19:30:47 +0000

    Minor label change

--- Plot6.R
+++ Plot6.R
@@ -2,18 +2,18 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 SCC.MotVh<- SCC$SCC[grep("Vehicles", SCC$SCC.Level.Two)]
-NEI.BL_LA.MotVh <- subset(NEI, (fips == "24510" | fips == "06037") & SCC %in% SCC.MotVh)
-NEI.BL_LA.MotVh$County[NEI.BL_LA.MotVh$fips == "24510"] <- "Baltimore City"
-NEI.BL_LA.MotVh$County[NEI.BL_LA.MotVh$fips == "06037"] <- "Los Angeles County"
-ytotal.BL_LA.MotVh <- aggregate(Emissions ~ County + year, NEI.BL_LA.MotVh, sum)
+NEI.BLLA.MotVh <- subset(NEI, (fips == "24510" | fips == "06037") & SCC %in% SCC.MotVh)
+NEI.BLLA.MotVh$County[NEI.BLLA.MotVh$fips == "24510"] <- "Baltimore City"
+NEI.BLLA.MotVh$County[NEI.BLLA.MotVh$fips == "06037"] <- "Los Angeles County"
+ytotal.BLLA.MotVh <- aggregate(Emissions ~ County + year, NEI.BLLA.MotVh, sum)
 
-hlines <- ytotal.BL_LA.MotVh[ytotal.BL_LA.MotVh$year == 2008, c(3,2,1)]
-ylabels <- trunc(c(max(ytotal.BL_LA.MotVh$Emissions), min(ytotal.BL_LA.MotVh$Emissions),
-                   ytotal.BL_LA.MotVh$Emissions[ytotal.BL_LA.MotVh$year == 1999 |
-                                                  ytotal.BL_LA.MotVh$year == 2008]), 0)  
+hlines <- ytotal.BLLA.MotVh[ytotal.BLLA.MotVh$year == 2008, c(3,2,1)]
+ylabels <- trunc(c(max(ytotal.BLLA.MotVh$Emissions), min(ytotal.BLLA.MotVh$Emissions),
+                   ytotal.BLLA.MotVh$Emissions[ytotal.BLLA.MotVh$year == 1999 |
+                                                  ytotal.BLLA.MotVh$year == 2008]), 0)  
 
 library(ggplot2)
-g <- ggplot(ytotal.BL_LA.MotVh, aes(year,Emissions))
+g <- ggplot(ytotal.BLLA.MotVh, aes(year,Emissions))
 
 png(filename="Plot6.png", width=640, bg="transparent")
   g + geom_line() + 
